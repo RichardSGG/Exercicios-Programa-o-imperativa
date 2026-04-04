79edf55385db8a5cb0342cb77c6ce36f79b91eff
Richard Silva <richarda50x@gmail.com>
2026-04-03 21:30:36 -0300

    Questão 1(Lista1ponto) Feita

---
+++ ProgImperativa/Lista1ponto/q1.java
@@ -0,0 +1,45 @@
+package Lista1ponto;
+import java.util.Scanner;
+
+public class q1 {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+
+    int d = sc.nextInt();
+    Consumo(d);
+        
+       
+    }
+
+    public static void Consumo(int d){
+        int [] dias = new int[d];
+        int consumototal = 0;
+        for(int i = 0; i < dias.length; i++){
+            dias[i] = sc.nextInt();
+            consumototal += dias[i];
+        }
+        float consumomedio = (consumototal / d);
+
+        int j = 0;
+        int contador = 0;
+
+        for(j = 0; j < dias.length; j++){
+            if (dias[j] > consumomedio){
+                contador ++;
+            }
+
+        }
+
+
+        System.out.println("Consumo Total: " + consumototal);
+        System.out.println("Consumo Médio: " + consumomedio);
+        System.out.println("Dias acima da média:  " + contador);
+       
+}
+    
+}
+
+
+
+
+
