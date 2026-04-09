5a473dbf5c8ff4a32e579d73c04d0089958c0643
Richard Silva <richarda50x@gmail.com>
2026-04-09 15:24:13 -0300

    Q10 feita

---
+++ ProgImperativa/Lista1ponto/Q10.java
@@ -0,0 +1,51 @@
+package Lista1ponto;
+import java.util.Scanner;
+
+public class Q10 {
+  static Scanner sc = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    Esta(sc.nextInt(), sc.nextInt());
+  }
+
+  public static void Esta(int l, int c) {
+    int[][] dias = new int[l][c];
+    int[] linhas = new int[l];
+    int[] colunas = new int[c];
+
+    for (int i = 0; i < l; i++) {
+      for (int j = 0; j < c; j++) {
+        dias[i][j] = sc.nextInt();
+        linhas[i] += dias[i][j];
+        colunas[j] += dias[i][j];
+      }
+    }
+    int maiorl = 0;
+    int j = 0;
+    for (int i = 0; i < l; i++) {
+      if (linhas[maiorl] < linhas[j]) {
+        maiorl = j;
+      }
+      j++;
+    }
+    int maiorc = 0;
+    j = 0;
+    for (int i = 0; i < c; i++) {
+      if (colunas[maiorc] < colunas[j]) {
+        maiorc = j;
+      }
+      j++;
+    }
+
+    for (int i = 0; i < l; i++) {
+      System.out.printf("Soma da Linha %d: %d%n", i, linhas[i]);
+    }
+
+    for (int i = 0; i < c; i++) {
+      System.out.printf("Soma da Coluna %d: %d%n", i, colunas[i]);
+    }
+    System.out.println("Linha com a maior soma: " + maiorl);
+    System.out.println("Linha com a maior soma: " + maiorc);
+
+  }
+}
